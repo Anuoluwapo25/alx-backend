b79e503eeb7b2a3f91b875e0c136d711afb4df48
Anuoluwapo25 <anuolwapoali25@gmail.com>
2024-06-26 15:11:52 +0000

    Initial commit

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" 0-main """
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ inherited fron BaseCaching class """
+
+    def put(self, key, item):
+        """Add or update a key-value pair in the cache."""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieve the value associated with a key from the cache."""
+        if key is not None key in self.cache_data:
+            return self.cache_data[key]
+        return None:
