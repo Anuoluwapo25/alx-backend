7ffdc8eba1a4d3a33706b5264889d2528855d0e3
Anuoluwapo25 <anuolwapoali25@gmail.com>
2024-06-26 16:31:12 +0000

    pop(FIFO)

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-""" 0-main """
+"""caching system"""
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ inherited fron BaseCaching class """
+    """
+    inherited fron BaseCaching class 
+    """
 
     def put(self, key, item):
-        """Add or update a key-value pair in the cache."""
-        if key is not None and item is not None:
+        """
+        Add or update a key-value pair in the cache.
+        """
+        if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """Retrieve the value associated with a key from the cache."""
-        if key is not None and key in self.cache_data:
-            return self.cache_data[key]
+        """
+        Retrieve the value associated with a key from the cache.
+        """
+        if key and key in self.cache_data:
+            return (self.cache_data.get(key))
         return None
